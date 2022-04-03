a31597a7635910250660e85015643c3215a61e96
t <t@LAPTOP-M8N6DE9U>
2022-04-03 21:02:19 +0200

    Add project files.

---
+++ kodeine/js/app.ts
@@ -0,0 +1,27 @@
+﻿class Greeter {
+    private element: HTMLElement;
+    private span: HTMLElement;
+    private timerToken: number;
+
+    constructor(element: HTMLElement) {
+        this.element = element;
+        this.element.innerHTML += "The time is: ";
+        this.span = document.createElement("span");
+        this.element.appendChild(this.span);
+        this.span.innerText = new Date().toUTCString();
+    }
+
+    public start() {
+        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+    }
+
+    public stop() {
+        clearTimeout(this.timerToken);
+    }
+}
+
+window.onload = () => {
+    const el = document.getElementById("content");
+    const greeter = new Greeter(el);
+    greeter.start();
+};
